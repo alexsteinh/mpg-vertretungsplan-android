5fe138958c27613ade53e25ac35de75860dfc246
alexsteinh <alexander@my-e-mail.net>
2020-09-17 00:51:53 +0200

    Fix custom themes for non-Android 11 devices

--- app/src/main/java/com/stonedroid/mpgvertretungsplan/theme/CustomTheme.java
+++ app/src/main/java/com/stonedroid/mpgvertretungsplan/theme/CustomTheme.java
@@ -6,6 +6,7 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.os.Build;
+import android.view.View;
 import android.view.WindowInsetsController;
 import com.stonedroid.mpgvertretungsplan.R;
 import com.stonedroid.mpgvertretungsplan.Utils;
@@ -20,21 +21,26 @@ public abstract class CustomTheme {
     }
 
     public void apply(boolean withActionBar) {
+        if (Build.VERSION.SDK_INT < 30) {
+            _applyLegacy(withActionBar);
+        } else {
+            _apply(withActionBar);
+        }
+    }
+
+    public void _applyLegacy(boolean withActionBar) {
         context.setTheme(withActionBar ? getResId() : getResIdNoActionBar());
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            WindowInsetsController controller = context.getWindow().getDecorView().getWindowInsetsController();
-            int flags = controller.getSystemBarsAppearance();
+            int flags = context.getWindow().getDecorView().getSystemUiVisibility();
 
             // Light status bar
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
                 if (isLight()) {
-                    controller.setSystemBarsAppearance(WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS,
-                            WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS);
+                    flags = Utils.addFlag(flags, View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
                 } else {
-                    if (Utils.hasFlag(flags, WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS)) {
-                        controller.setSystemBarsAppearance(0,
-                                WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS);
+                    if (Utils.hasFlag(flags, View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR)) {
+                        flags = Utils.removeFlag(flags, View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
                     }
                 }
             }
@@ -42,13 +48,11 @@ public abstract class CustomTheme {
             // Light navigation bar
             if (Build.VERSION.SDK_INT >= 26) {
                 if (isLight()) {
-                    controller.setSystemBarsAppearance(WindowInsetsController.APPEARANCE_LIGHT_NAVIGATION_BARS,
-                            WindowInsetsController.APPEARANCE_LIGHT_NAVIGATION_BARS);
+                    flags = Utils.addFlag(flags, View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR);
                     context.getWindow().setNavigationBarColor(Color.WHITE);
                 } else {
-                    if (Utils.hasFlag(flags, WindowInsetsController.APPEARANCE_LIGHT_NAVIGATION_BARS)) {
-                        controller.setSystemBarsAppearance(0,
-                                WindowInsetsController.APPEARANCE_LIGHT_NAVIGATION_BARS);
+                    if (Utils.hasFlag(flags, View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR)) {
+                        flags = Utils.removeFlag(flags, View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR);
                     }
 
                     context.getWindow().setNavigationBarColor(Color.BLACK);
@@ -68,11 +72,50 @@ public abstract class CustomTheme {
                     context.getWindow().setNavigationBarDividerColor(Color.BLACK);
                 }
             } else {
-                int bmpRes = Build.VERSION.SDK_INT >= 25 ? R.mipmap.ic_launcher_round : R.mipmap.ic_launcher;
-                context.setTaskDescription(new ActivityManager.TaskDescription(null, bmpRes,
+                Bitmap bmp = BitmapFactory.decodeResource(context.getResources(),
+                        Build.VERSION.SDK_INT >= 25 ? R.mipmap.ic_launcher_round : R.mipmap.ic_launcher);
+
+                context.setTaskDescription(new ActivityManager.TaskDescription(null, bmp,
                         Utils.getThemePrimaryColor(context)));
+
+                bmp.recycle();
             }
+
+            context.getWindow().getDecorView().setSystemUiVisibility(flags);
         }
+    }
+
+    public void _apply(boolean withActionBar) {
+        context.setTheme(withActionBar ? getResId() : getResIdNoActionBar());
+        WindowInsetsController controller = context.getWindow().getDecorView().getWindowInsetsController();
+        int flags = controller.getSystemBarsAppearance();
+
+        if (isLight()) {
+            controller.setSystemBarsAppearance(WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS,
+                    WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS);
+            controller.setSystemBarsAppearance(WindowInsetsController.APPEARANCE_LIGHT_NAVIGATION_BARS,
+                    WindowInsetsController.APPEARANCE_LIGHT_NAVIGATION_BARS);
+
+            context.getWindow().setNavigationBarColor(Color.WHITE);
+            context.getWindow().setNavigationBarDividerColor(Color.parseColor("#e0e0e0"));
+        } else {
+            if (Utils.hasFlag(flags, WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS)) {
+                controller.setSystemBarsAppearance(0,
+                        WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS);
+            }
+
+            if (Utils.hasFlag(flags, WindowInsetsController.APPEARANCE_LIGHT_NAVIGATION_BARS)) {
+                controller.setSystemBarsAppearance(0,
+                        WindowInsetsController.APPEARANCE_LIGHT_NAVIGATION_BARS);
+            }
+
+            context.getWindow().setNavigationBarColor(Color.BLACK);
+            context.getWindow().setNavigationBarDividerColor(Color.BLACK);
+        }
+
+        context.setTaskDescription(new ActivityManager.TaskDescription(null,
+                R.mipmap.ic_launcher_round,
+                Utils.getThemePrimaryColor(context)));
     }
 
     public abstract String getName();
